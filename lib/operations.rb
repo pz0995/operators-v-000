63a09c89bd7aaf7093bf9effd136902caf896e02
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 20:21:37 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed  < 40
+  	 return true
+  elsif speed > 60
+   return true
+ else return false
 end
 
-
+end
 
 def not_safe?(speed)
-	
+  speed < 40 ? true : false ||  speed > 60 ? true : false
 end
-	
-
-
