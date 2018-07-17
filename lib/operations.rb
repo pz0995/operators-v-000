683de31d0ea2e730eb1f030815efb31fc16277ac
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 20:20:40 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,11 @@
 def unsafe?(speed)
   if speed  < 40
   	 return true
-  end
-
-
-if speed > 60
+  elsif speed > 60
    return true
+ else return false
 end
 
-if speed < 40 && speed 60 >
-  return false
-end
 
 
 def not_safe?(speed)
